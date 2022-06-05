a20e964d6f43eacb77da561a6d6ea29a524caaab
ronran1 <rohan.rani5444@gmail.com>
2022-06-05 11:53:56 -0700

    py

---
+++ Python/8kyu/2022-06-04.py
@@ -0,0 +1,4 @@
+#Can we divide it?
+def is_divide_by(number, a, b):
+    return number % a == 0 and number % b == 0
+
---
+++ Python/8kyu/2022-06-05.py
@@ -0,0 +1,8 @@
+#Keephydrated
+import math
+def litres(time):
+    return math.floor(time * 0.5)
+
+#Convert num to string
+def number_to_string(num):
+    return str(num)
